e9ebfe82d027a1e8f56437529fabefb79bf30217
Alexandre Thebault <alexandre.thebault77@gmail.com>
2020-04-24 12:11:17 +0200

    ajout de l'upload des metadonnees

--- src/shared/models/FileModel.ts
+++ src/shared/models/FileModel.ts
@@ -1,13 +1,20 @@
 import { DocumentType, EnumTypeValue } from 'src/shared/models/document-type.model';
+import { LongTapEvent } from 'ag-grid-community';
 
 
 export class FileModel {
+  public id: Number;
+  public name: String;
+  public size: Number;
+  public type: String;
   public documentType: DocumentType;
   public company: String;
-  public job: String;
+  public workplace: String;
   public date: Date;
-  public file: File;
-
+  public grossSalary: Number;
+  public netSalary: Number;
+  public fileContent: String;
+  
   constructor() {
 
   }
